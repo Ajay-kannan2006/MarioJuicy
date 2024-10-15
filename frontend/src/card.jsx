e2953a90cd4aac3001dfc8bb8cebec5941fae9a1
balachandar <balachandhar@gmail.com>
2024-10-15 18:18:40 +0530

    card dynamic values

--- frontend/src/card.jsx
+++ frontend/src/card.jsx
@@ -1,19 +1,23 @@
 import './card.css';
 
+  let item_name="choco";
+  let item_quantity=10;
+  let item_price=10;
+  
+
 function Card() {
   return (
     <div className="cart">
       <div className="image_cart"></div>
       <div className="cart_content">
-        {/* <h4 style={{ color: '#037014' }}>Category: Ice Cream</h4> */}
-        <h4 style={{ color: '#FA1041' }}>Chocolate Ice Cream</h4>
+        <h4 style={{ color: '#FA1041' }}>{item_name} Ice Cream</h4>
         <div className="quantity" style={{ color: '#037014' }}>
           <h4>Quantity:</h4>
-          <p id="Chocolate_ice_cream_quantity" style={{ color: '#037014' }}>10</p>
+          <p id="Chocolate_ice_cream_quantity" style={{ color: '#037014' }}>{item_quantity}</p>
         </div>
         <div className="cart_footer">
           <button>Add Cart</button>
-          <p>Price: $10</p>
+          <p>Price: ${item_price}</p>
         </div>
       </div>
     </div>
