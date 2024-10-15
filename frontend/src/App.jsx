0b7f0880e2277ba36a836a7334adcb75b7ec120e
Ajay-kannan2006 <ajaykannan1001200635@gmail.com>
2024-10-15 20:01:37 +0530

    sent the details from express and rendered them with react Card component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,27 @@
-import './App.css'
-import Card from './card'
+import './App.css';
+import Card from './card';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [items, setItem] = useState([]);
 
+  useEffect(() => {
+    axios.get("http://localhost:8080/api")
+      .then((res) => {
+        setItem(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
   return (
     <div className='cards'>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
+      {/* <p>{items[0].item_name}</p> */}
+      {items.map((item, index) => (
+        <Card key={index} item_name={item.item_name} item_quantity={item.item_quantity} item_price={item.item_price} />
+      ))}
     </div>
-    
   )
 }
 
