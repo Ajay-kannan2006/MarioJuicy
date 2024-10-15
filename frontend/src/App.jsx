dcf99ce82667d287846505e8e34376f0b149911b
balachandar <balachandhar@gmail.com>
2024-10-15 19:02:42 +0530

    component filter_bar added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Card from './card'
-
+import Filter_bar from './Filter_bar'
 function App() {
 
   return (
@@ -9,7 +9,7 @@ function App() {
       <Card/>
       <Card/>
       <Card/>
-      <Card/>
+      <Filter_bar/>
     </div>
     
   )
