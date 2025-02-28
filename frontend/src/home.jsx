0c34b8dc64e23b35f026af97601830f481198dd9
balachandar <balachandhar@gmail.com>
2025-02-28 16:08:53 +0530

    banner added

--- frontend/src/home.jsx
+++ frontend/src/home.jsx
@@ -1,11 +1,16 @@
 import Header from './HomeScreen/Header'
 import HomeScreenOffers from './HomeScreen/HomeScreenOffers'
 import NavBar from './nav'
+import HomeScreenOffers from './HomeScreen/HomeScreenOffers'
+import Banner from './HomeScreen/Banner'
+
 const Home=()=>{
     return (
         <div>
             <Header/>
             <NavBar/>
+            <Banner/>
+            <HomeScreenOffers/>
         </div>
         
         
